8fde223a5d731415d5cf6ed70e0e80dfcb368b02
Cristian Emmanuel Lorenzo <134024355+CristianLorenzo11@users.noreply.github.com>
2024-03-26 15:46:42 -0300

    Update script.js
    segimos editando

--- script.js
+++ script.js
@@ -55,12 +55,12 @@ function mostrarImagenConejo() {
     imagenConejo.src = "CONEJO1.jpeg"; // Ruta de la imagen del conejo
     imagenConejo.alt = "Imagen del conejo";
     
+    
+    document.body.appendChild(imagenConejo); // Agrega la imagen al cuerpo del documento
     // Aplica estilos CSS para hacer que la imagen sea redondeada
     imagenConejo.style.borderRadius = "50%";
    // imagenConejo.style.width = "400px"; // Modifica el tamaño de la imagen si es necesario
     //imagenConejo.style.height = "400px"; // Modifica el tamaño de la imagen si es necesario
-    
-    document.body.appendChild(imagenConejo); // Agrega la imagen al cuerpo del documento
 }
 
 
