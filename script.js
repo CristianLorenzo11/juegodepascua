085c792a39bddea681e3fa62722818f3e1806c19
Cristian Emmanuel Lorenzo <134024355+CristianLorenzo11@users.noreply.github.com>
2024-03-28 13:58:12 -0300

    SE AGREGA TIEMPO

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let nivelDificultad = "medio"; // Nivel de dificultad por defecto
+let tiempoInicio; // Variable para almacenar el tiempo de inicio del juego
+let tiempoFin; // Variable para almacenar el tiempo de fin del juego
 
 document.getElementById("selectDificultad").addEventListener("change", function() {
   nivelDificultad = this.value;
@@ -31,7 +33,6 @@ function generarNumeroAleatorio() {
     console.log("Número aleatorio generado:", numeroazar); // Agrega este console.log para ver el número aleatorio generado
   }
   
-
 // Variable para almacenar el nombre y apellido del jugador
 let nombreApellido = "";
 let numeroEntrada = document.getElementById("numeroEntrada"); // aca se ingresa el numero que la persona coloca 
@@ -50,83 +51,94 @@ function chequearResultado() {
       document.getElementById("numeroEntrada").value = "";
       return;
   }
-    // Si ya se ha adivinado, no hacer nada
-    if (adivinado) {
-      return;
-    }
-    intentos++;
-    intento.textContent = intentos;
-  
-    // Obtener el valor ingresado en el campo de nombre y apellido
-    nombreApellido = document.getElementById("nombreApellido").value;
-    let numeroingresado = parseInt(numeroEntrada.value);
-  
-    // Validar el rango del número ingresado según el nivel de dificultad
-    if (nivelDificultad === "facil" && (numeroingresado < 1 || numeroingresado > 50)) {
-      mensaje.textContent = "Por favor ingresa un número válido entre 1 y 50";
-      mensaje.style.color = "black";
-      return;
-    } else if (nivelDificultad === "medio" && (numeroingresado < 1 || numeroingresado > 100)) {
-      mensaje.textContent = "Por favor ingresa un número válido entre 1 y 100";
-      mensaje.style.color = "black";
-      return;
-    } else if (nivelDificultad === "dificil" && (numeroingresado < 1 || numeroingresado > 1000)) {
-      mensaje.textContent = "Por favor ingresa un número válido entre 1 y 1000";
-      mensaje.style.color = "black";
-      return;
-    }
-  
-    // Comparar el número ingresado con el número aleatorio
-    if (numeroingresado < numeroazar) {
-      mensaje.textContent = "El numero que dijiste es muy Bajo,  En la mochila del conejo hay mayor cantidad de huevitos";
-      return;
-    }
-    if (numeroingresado > numeroazar) {
-      mensaje.textContent = "El numero que dijiste es muy Alto,  En la mochila del conejo hay menos cantidad de huevitos";
-      return;
-    }
-  
-    // Si el jugador gana, mostrar un mensaje con su nombre y apellido
-    if (numeroingresado === numeroazar) {
-      adivinado = true; // Establecer adivinado en true
-      let mensajeNivel = "";
-      if (nivelDificultad === "facil") {
-          mensajeNivel = "Fácil";
-      } else if (nivelDificultad === "medio") {
-          mensajeNivel = "Medio";
-      } else if (nivelDificultad === "dificil") {
-          mensajeNivel = "Difícil";
-      }
-      mensaje.textContent = "¡Felicidades, " + nombreApellido + ", Adivinaste la cantidad de huevitos en el Nivel " + mensajeNivel + "!";
-      mensaje.style.color = "green";
-      mensaje.style.fontSize = "24px"; // Cambiar tamaño de letra si deseas
-      mostrarImagenConejo(); // Llama a la función para mostrar la imagen del conejo
-      numeroEntrada.disabled = true;
-      document.getElementById("selectDificultad").disabled = true; // Deshabilitar el selector de dificultad
-    
-      return;
-    }
-  }
-  
 
-  function mostrarImagenConejo() {
-    let imagenConejo = document.createElement("img");
-    imagenConejo.src = "CONEJO1.jpeg"; // Ruta de la imagen del conejo
-    imagenConejo.alt = "Imagen del conejo";
-    
-    // Aplica estilos CSS para hacer que la imagen sea redondeada
-    imagenConejo.style.borderRadius = "50%";
-    
-    // Agregar la imagen al cuerpo del documento
-    document.body.appendChild(imagenConejo);
-    
-    // Esperar un breve momento para que la imagen se agregue completamente al DOM
-    setTimeout(function() {
-      // Hacer scroll para mostrar la imagen del conejo
-      imagenConejo.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }, 100);
+  // Iniciar el cronómetro al comenzar el juego
+  if (!tiempoInicio) {
+    tiempoInicio = new Date();
   }
-        
+
+  // Si ya se ha adivinado, no hacer nada
+  if (adivinado) {
+    return;
+  }
+  intentos++;
+  intento.textContent = intentos;
+
+  // Obtener el valor ingresado en el campo de nombre y apellido
+  nombreApellido = document.getElementById("nombreApellido").value;
+  let numeroingresado = parseInt(numeroEntrada.value);
+
+  // Validar el rango del número ingresado según el nivel de dificultad
+  if (nivelDificultad === "facil" && (numeroingresado < 1 || numeroingresado > 50)) {
+    mensaje.textContent = "Por favor ingresa un número válido entre 1 y 50";
+    mensaje.style.color = "black";
+    return;
+  } else if (nivelDificultad === "medio" && (numeroingresado < 1 || numeroingresado > 100)) {
+    mensaje.textContent = "Por favor ingresa un número válido entre 1 y 100";
+    mensaje.style.color = "black";
+    return;
+  } else if (nivelDificultad === "dificil" && (numeroingresado < 1 || numeroingresado > 1000)) {
+    mensaje.textContent = "Por favor ingresa un número válido entre 1 y 1000";
+    mensaje.style.color = "black";
+    return;
+  }
+
+  // Comparar el número ingresado con el número aleatorio
+  if (numeroingresado < numeroazar) {
+    mensaje.textContent = "El numero que dijiste es muy Bajo,  En la mochila del conejo hay mayor cantidad de huevitos";
+    return;
+  }
+  if (numeroingresado > numeroazar) {
+    mensaje.textContent = "El numero que dijiste es muy Alto,  En la mochila del conejo hay menos cantidad de huevitos";
+    return;
+  }
+
+  // Si el jugador gana, mostrar un mensaje con su nombre y apellido
+  if (numeroingresado === numeroazar) {
+    adivinado = true; // Establecer adivinado en true
+    // Detener el cronómetro al ganar
+    tiempoFin = new Date();
+    let tiempoTotal = tiempoFin - tiempoInicio; // Calcular tiempo transcurrido en milisegundos
+    let tiempoSegundos = Math.floor(tiempoTotal / 1000); // Convertir milisegundos a segundos
+    let minutos = Math.floor(tiempoSegundos / 60); // Calcular minutos
+    let segundos = tiempoSegundos % 60; // Calcular segundos restantes
+    let tiempoMensaje = `Tiempo: ${minutos} minutos y ${segundos} segundos`;
+
+    let mensajeNivel = "";
+    if (nivelDificultad === "facil") {
+      mensajeNivel = "Fácil";
+    } else if (nivelDificultad === "medio") {
+      mensajeNivel = "Medio";
+    } else if (nivelDificultad === "dificil") {
+      mensajeNivel = "Difícil";
+    }
+    mensaje.textContent = `¡Felicidades, ${nombreApellido}! Adivinaste la cantidad de huevitos en el Nivel ${mensajeNivel} en ${tiempoMensaje}!`;
+    mensaje.style.color = "green";
+    mensaje.style.fontSize = "24px"; // Cambiar tamaño de letra si deseas
+    mostrarImagenConejo(); // Llama a la función para mostrar la imagen del conejo
+    numeroEntrada.disabled = true;
+    document.getElementById("selectDificultad").disabled = true; // Deshabilitar el selector de dificultad
+    return;
+  }
+}
+
+function mostrarImagenConejo() {
+  let imagenConejo = document.createElement("img");
+  imagenConejo.src = "CONEJO1.jpeg"; // Ruta de la imagen del conejo
+  imagenConejo.alt = "Imagen del conejo";
+  
+  // Aplica estilos CSS para hacer que la imagen sea redondeada
+  imagenConejo.style.borderRadius = "50%";
+  
+  // Agregar la imagen al cuerpo del documento
+  document.body.appendChild(imagenConejo);
+  
+  // Esperar un breve momento para que la imagen se agregue completamente al DOM
+  setTimeout(function() {
+    // Hacer scroll para mostrar la imagen del conejo
+    imagenConejo.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }, 100);
+}
 
 function checkEnter(event) {
   if (event.key === "Enter") {
@@ -135,3 +147,4 @@ function checkEnter(event) {
 }
 
 generarNumeroAleatorio(); // Genera el número al azar al cargar la página
+
